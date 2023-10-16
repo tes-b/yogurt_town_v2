e096e828b323cc1bf5f64f4a1702455dffd6e371
tes-b <tsyi0313@gmail.com>
2023-10-17 00:05:54 +0900

    remove footer

--- YogurtTown_v2/wwwroot/js/canvas.js
+++ YogurtTown_v2/wwwroot/js/canvas.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById('canvas');
 
 var header = document.body.getElementsByTagName('header')[0];
-var footer = document.body.getElementsByTagName('footer')[0];
+//var footer = document.body.getElementsByTagName('footer')[0];
 var bodyheight = document.body.clientHeight;
 
-canvas.setAttribute("Height", bodyheight - header.clientHeight - footer.clientHeight);
+canvas.setAttribute("Height", bodyheight - header.clientHeight);
 canvas.setAttribute("Width", window.innerWidth);
 
 var ctx = canvas.getContext('2d');
